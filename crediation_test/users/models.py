3bd77ac214fdaafec38d1c65da8080fdab0b0d6f
smithjilks <smithjilks@gmail.com>
2021-12-04 09:41:56 +0300

    Create user model

--- crediation_test/users/models.py
+++ crediation_test/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+        firstname = models.CharField(max_length=20)
+        lastname = models.CharField(max_length=20)
+        email = models.EmailField()
+        id = models.UUIDField(primary_key=True)
+
+        def __str__(self):
+            return "({}) {}".format(self.id, self.__dict__)
+
